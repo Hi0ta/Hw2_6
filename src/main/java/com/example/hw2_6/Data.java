8faa91c67b5ab0f7eb9988bd6b6abd3ae112e168
Ann <niyta@mail.ru>
2022-10-13 17:34:39 +0500

    ДЗ 1 доработано

--- src/main/java/com/example/hw2_6/Data.java
+++ src/main/java/com/example/hw2_6/Data.java
@@ -4,7 +4,7 @@ public class Data {
     private String login;
     private String password;
     private String confirmPassword;
-    private static final String VALID_CHARACTERS = "abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
+    private static final String VALID_CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_";
 
     public Data(String login, String password, String confirmPassword) {
         this.setLogin(login);
@@ -16,7 +16,7 @@ public class Data {
     public String getConfirmPassword() {return confirmPassword;}
 
     public void setLogin(String login) {
-        if (0 < login.length() && login.length() <= 20) {
+        if (0 < login.length() && login.length() <= 20 && valid(login)) {
             this.login = login;
             System.out.println("true");
             } else{
@@ -28,14 +28,14 @@ public class Data {
             }
     }
     public void setPassword(String password) {
-        if (0 < password.length() && password.length() <= 20) {
+        if (0 < password.length() && password.length() <= 20 && valid(password)) {
             this.password = password;
             System.out.println("true");
         } else {
             try {
                 throw new WrongPasswordException("Пароль должен быть заполнен!! И содержать в себе только латинские буквы, цифры или знак подчеркивания. Длиной не более 20 символов");
-            } catch (WrongPasswordException ex) {
-                System.out.println(ex.getMessage());
+            } catch (WrongPasswordException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
@@ -46,15 +46,16 @@ public class Data {
         } else {
             try {
                 throw new WrongPasswordException("Подтверждающий пароль должен быть заполнен!! И полностью совпадать с паролем");
-            } catch (WrongPasswordException exept) {
-                System.out.println(exept.getMessage());
+            } catch (WrongPasswordException e) {
+                System.out.println(e.getMessage());
             }
         }
     }
     private static boolean valid(String s) {
         for (int i = 0; i < s.length(); i++) {
-            if (!VALID_CHARACTERS.contains(String.valueOf(s.charAt(i)))) ;
-            return false;
+            if (!VALID_CHARACTERS.contains(String.valueOf(s.charAt(i)))) {
+                return false;
+            }
         }
         return true;
     }
--- src/main/java/com/example/hw2_6/Main.java
+++ src/main/java/com/example/hw2_6/Main.java
@@ -2,12 +2,10 @@ package com.example.hw2_6;
 
 public class Main {
 
+
     public static void main(String[] args)  throws WrongPasswordException, WrongLoginException {
 
-        Data user = new Data("35!!!34d", "5789", "5789");
-
-
-
+        Data user = new Data("354njn_AABkjd", "256AA", "256AA");
 
     }
 
